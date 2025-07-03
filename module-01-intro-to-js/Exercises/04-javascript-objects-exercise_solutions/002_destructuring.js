ce1c25fa5b39aec893d42c0a6633469c1cd5f810
Renke Brixel <renke.brixel@wbscodingschool.com>
2025-07-03 16:02:04 +0200

    add most JS basics solutions, except the last 3

---
+++ module-01-intro-to-js/Exercises/04-javascript-objects-exercise_solutions/002_destructuring.js
@@ -0,0 +1,37 @@
+// You can work here or download the template!
+// Initial array
+const fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// Initial object
+const person = {
+  name: 'John Doe',
+  age: 30,
+  address: {
+    city: 'New York',
+    zip: '10001',
+  },
+};
+
+const [fruit1, fruit2] = fruits;
+console.log(fruit1); // apple
+console.log(fruit2); // banana
+
+const [firstFruit, , thirdFruit] = fruits;
+console.log(firstFruit); // apple
+console.log(thirdFruit); // cherry
+
+const { name, age } = person;
+console.log(name); // John Doe
+console.log(age); // 30
+
+const {
+  address: { city },
+} = person;
+console.log(city); // New York
+
+// Initial function
+function displayPerson({ name, age }) {
+  console.log(`Name: ${name}, Age: ${age}`);
+}
+// Call the function
+displayPerson(person);
